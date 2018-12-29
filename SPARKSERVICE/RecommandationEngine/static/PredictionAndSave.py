53d4cfb13e435013b471f79c8c617ab0ff7d76f2
root <root@sparktest-m.us-east4-a.c.finaldeploy.internal>
2018-12-29 08:58:32 +0000

    Updating TainModel and Prediction  Script

--- SPARKSERVICE/RecommandationEngine/static/PredictionAndSave.py
+++ SPARKSERVICE/RecommandationEngine/static/PredictionAndSave.py
@@ -3,12 +3,19 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.mllib.recommendation import ALS
 from pyspark.mllib.recommendation import MatrixFactorizationModel
+from pymongo import MongoClient
+
+Path1 = "/TEST/MODEL1/"
+Path2 = "/TEST/MODEL2/"
+
+DB = "PATHDB"
+PathCol = "PATHCOL"
 
 
 
 sc = SparkContext(appName = "TrainedModel")
-spark = SparkSession.builder.appName('recommend').config("spark.mongodb.input.uri","mongodb://10.150.0.4:27017/csvmongo.colcsv").getOrCreate()
-data1 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://10.150.0.4:27017/csvmongo.colcsv").load()
+spark = SparkSession.builder.appName('recommend').config("spark.mongodb.input.uri","mongodb://10.150.0.3:27017/newdb123.rating").getOrCreate()
+data1 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://10.150.0.3:27017/newdb123.rating").load()
 
 data1.createOrReplaceTempView("temp1")
 data = spark.sql("SELECT userID,tutID,rating from temp1")
@@ -16,8 +23,11 @@ data = spark.sql("SELECT userID,tutID,rating from temp1")
 test.createOrReplaceTempView("tmp2")
 tstrdd = spark.sql("SELECT userID,tutID from tmp2")
 
-userID = int(sys.argv[0])
-
+print("UserID\n")
+print(sys.argv)
+userID = int(sys.argv[1])
+print("userID  ========== "+str(userID)+"\n")
+print("Start Recommandation\n")
 tst = tstrdd.rdd.map(list)
 tmp = tst.filter(lambda x:x[0]==userID).map(lambda x:(x[1])).collect()
 try1 = tst.filter(lambda rating: rating[1] not in tmp).map(lambda x:(userID,x[1])).distinct()
@@ -29,19 +39,38 @@ print("\nGet cleaning is done\n")
 
 #print("--"*50)
 #print("\nMdel tarined\n")
-model = MatrixFactorizationModel.load(sc, "hdfs://sparkm-m:8020/TEST/MODEL")
+
+client = MongoClient('mongodb://10.150.0.3:27017/')
+db = client[DB]
+res = db[PathCol].find()
+
+tmppath = ""
+for doc in res:
+    tmppath =  str(doc['path'])
+    break
+print "Get Path From DB"+tmppath+"\n"
+model = MatrixFactorizationModel.load(sc, tmppath)
 result = model.predictAll(try1)
 lst_prediction = result.takeOrdered(10,key=lambda x: -x[2])
 print("--"*50)
 print("\nGot all required result\n")
 print("\n"+str(lst_prediction)+"\n")
 
+
+
+client = MongoClient('mongodb://10.150.0.3:27017/')
+db = client["RecommandationDB"]
+res = db["RecommandationCol"]
+
+print "Writing to DB"
+
 lst_tmp = []
 for i in lst_prediction:
     print("UserID: "+str(i.user)+"TutID: "+str(i.product)+"Rating: "+str(i.rating)+"\n")
-    tmp_tuple = (i.user,i.product,i.rating)
+    db["RecommandationCol"].insert({"userID":str(i.user),"tutID":str(i.product),"rating":str(i.rating)})
+    tmp_tuple = (str(i.user),str(i.product),str(i.rating))
     lst_tmp.append(tmp_tuple)
 
-print("\n"+str(lst_tmp)+" Writing to database start\n")
-recomandation_data = spark.createDataFrame(lst_tmp,["usrID","TutID","rating"])
-recomandation_data.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").option("uri","mongodb://10.150.0.4:27017").option("database","recomandationres").option("collection", "result").save()
+print("\n"+str(lst_tmp)+" Writing to database over\n")
+#recomandation_data = spark.createDataFrame(lst_tmp,["usrID","TutID","rating"])
+#recomandation_data.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").option("uri","mongodb://10.150.0.3:27017").option("database","RecommandationDB").option("collection", "RecommandationCol").save()
